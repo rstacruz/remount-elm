074b21e79f9d64638d814d6b808954dfc4102a65
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-02-17 21:41:03 +0800

    Map out how updates will work

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Mount Elm apps as web components using [Remount]
 
+_(WIP prrof-of-concept)_
+
 [Remount]: https://github.com/rstacruz/remount
--- src/index.js
+++ src/index.js
@@ -8,15 +8,24 @@ const define = (components, defaults = {}) => {
 }
 
 const Adapter = {
-  mount({ component }, mountPoint, props) {
+  mount(spec, mountPoint, props) {
     // This function will be called on the first appearance of the custom
     // element.
-    component.embed(mountPoint, props)
+    const { component } = spec
+    const app = component.init({ node: mountPoint, flags: props })
+
+    // Store the Elm instance so we can communicate with the ports later
+    mountPoint._elmApp = app
   },
+
   update({ component }, mountPoint, props) {
     // This function will be called on any subsequent updates afterwards (ie,
     // if attributes were changed).
+    // TODO: ports
+    // https://guide.elm-lang.org/interop/ports.html
+    // mountPoint._elmApp.ports[propKey].send(propValue)
   },
+
   unmount({ component }, mountPoint) {
     // This function will be called when a custom element is removed from the
     // DOM (eg, `parent.removeChild()`).
